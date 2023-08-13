64016583c39660a5c757a66f15a69c01ef63bcf0
Adam Bossy <adambossy@gmail.com>
2023-08-13 15:56:30 -0400

    Successfully extract a single leaf node

--- decompose.dart
+++ decompose.dart
@@ -3,13 +3,19 @@ import 'dart:io';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
 
 class Node {
   final String name;
   final Node? parent;
   final List<Node> children;
+  final String? source;
+  final int? offset;
+  final int? length;
 
-  Node(this.name, {this.parent}) : children = [];
+  Node(this.name, {this.parent, this.source, this.offset, this.length})
+      : children = [];
 }
 
 /*
@@ -62,26 +68,18 @@ class WidgetStateVisitor extends RecursiveAstVisitor<void> {
 
   @override
   void visitClassDeclaration(ClassDeclaration node) {
-    currentNode = Node(node.name.toString(), parent: currentNode);
-    root.children.add(currentNode);
     super.visitClassDeclaration(node);
   }
 
   @override
   void visitMethodDeclaration(MethodDeclaration node) {
     if (node.name.toString() == 'build') {
-      final childNode = Node(node.name.toString(), parent: currentNode);
-      currentNode.children.add(childNode);
-      currentNode = childNode;
       super.visitMethodDeclaration(node);
     }
   }
 
   @override
   void visitReturnStatement(ReturnStatement node) {
-    final childNode = Node('return', parent: currentNode);
-    currentNode.children.add(childNode);
-    currentNode = childNode;
     node.visitChildren(MethodInvocationVisitor(currentNode));
 
     // It's important to forgo the super() call to avoid matching nested return statements
@@ -100,7 +98,11 @@ class MethodInvocationVisitor extends SimpleAstVisitor<void> {
 
   @override
   void visitMethodInvocation(MethodInvocation node) {
-    final subtree = Node(node.methodName.toString(), parent: currentNode);
+    final subtree = Node(node.methodName.toString(),
+        parent: currentNode,
+        source: node.toSource(),
+        offset: node.offset,
+        length: node.length);
     buildTree(subtree, node);
     currentNode.children.add(subtree);
   }
@@ -152,9 +154,84 @@ void main(List<String> arguments) async {
   parseResult.unit.accept(visitor);
 
   printTree(visitor.root);
+
+  final file = File(fileName);
+  await file.writeAsString(extractWidget(code, visitor.root),
+      mode: FileMode.write);
+}
+
+String widgetMethod(String body) {
+  /* 
+  Widget <nameOfMethod><uniqueIdentifier>(BuildContext context} {
+    ...
+  }
+  */
+  final widget = Method((b) => b
+    ..name = 'widget123'
+    ..returns = refer('Widget')
+    ..body = Code('return $body;'));
+  final emitter = DartEmitter();
+  final code = DartFormatter().format('${widget.accept(emitter)}');
+  print(code);
+
+  return code;
+}
+
+String extractWidget(String code, Node node) {
+  // Navigate to the first leaf node
+  Node currentNode = node;
+  while (currentNode.children.isNotEmpty) {
+    currentNode = currentNode.children.elementAt(0);
+  }
+
+  print('currentNode ${currentNode.name}');
+
+  String replacement = 'widget123()';
+  String newMethod = widgetMethod(currentNode.source!);
+  String newCode = code.substring(0, currentNode.offset) +
+      replacement +
+      code.substring(currentNode.offset! + currentNode.length!) +
+      newMethod;
+
+  final _newCode = DartFormatter().format(newCode);
+
+  print(_newCode);
+
+  return _newCode;
+}
+
+class Replacer {
+  Replacer(String input)
+      : _string = input,
+        _max = input.length;
+
+  String get string => _string;
+  String _string;
+  final int _max;
+
+  var _offset = 0;
+  var _tabu = 0;
+
+  void replace(int start, int end, String replacement) {
+    assert(start >= 0, 'start must be ≥ 0');
+    assert(end >= end, 'end must be ≥ start');
+    assert(end <= _max, 'end must be ≤ string length');
+    assert(start >= _tabu, 'replacement must not overlap');
+
+    final length = end - start;
+    final nstart = start + _offset;
+    _string = _string.substring(0, nstart) +
+        replacement +
+        _string.substring(nstart + length);
+    _offset += replacement.length;
+    _offset -= length;
+    _tabu = end;
+  }
 }
 
 void printTree(Node node, [int indent = 0]) {
-  print('${' ' * indent}${node.name}');
-  node.children.forEach((child) => printTree(child, indent + 2));
+  print('${' ' * indent}${node.name} | ${node.offset} | ${node.length}');
+  for (var child in node.children) {
+    printTree(child, indent + 2);
+  }
 }

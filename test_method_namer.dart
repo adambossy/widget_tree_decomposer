a971c7349c7843f61a695af3f8985fcb04345c39
Adam Bossy <adambossy@gmail.com>
2023-08-14 11:10:45 -0400

    Tests for parsing method names correctly

--- test_method_namer.dart
+++ test_method_namer.dart
@@ -11,8 +11,44 @@ void main() {
     expect(namer.methodNames, {});
   });
 
-  test(
-      'method namer parses method names single factored function', () async {});
+  test('method namer parses method names with one factored function', () async {
+    String code =
+        await File('test/test_one_factored_method.dart').readAsString();
+    MethodNamer namer = MethodNamer(code);
+    expect(namer.methodNames, {'Grault': 1});
+  });
+
+  test('method namer parses method names with two factored functions',
+      () async {
+    String code =
+        await File('test/test_two_factored_methods.dart').readAsString();
+    MethodNamer namer = MethodNamer(code);
+    expect(namer.methodNames, {'Grault': 1, 'Corge': 1});
+  });
+
+  test('method namer parses method names with three factored functions',
+      () async {
+    String code =
+        await File('test/test_three_factored_methods.dart').readAsString();
+    MethodNamer namer = MethodNamer(code);
+    expect(namer.methodNames, {'Grault': 1, 'Corge': 2});
+  });
+
+  test('method namer parses method names with four factored functions',
+      () async {
+    String code =
+        await File('test/test_four_factored_methods.dart').readAsString();
+    MethodNamer namer = MethodNamer(code);
+    expect(namer.methodNames, {'Grault': 1, 'Corge': 2, 'Qux': 1});
+  });
+
+  test('method namer parses method names with five factored functions',
+      () async {
+    String code =
+        await File('test/test_five_factored_methods.dart').readAsString();
+    MethodNamer namer = MethodNamer(code);
+    expect(namer.methodNames, {'Grault': 1, 'Corge': 2, 'Qux': 1, 'Foobar': 1});
+  });
 
 //   test('method namer names methods correctly', () async {
 //     String code = await File('test_file.dart').readAsString();
